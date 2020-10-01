4d5c809eef44db84abecb9909227fb477e6ed844
rmzNadir <dgzrz99@gmail.com>
2020-09-30 20:29:15 -0500

    :construction:

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Suspense, useState, useEffect } from "react";
 import { Canvas } from "react-three-fiber";
 import "./App.css";
 import Model from "./components/Model";
+import Background from "./components/Background"
 import Loading from "./components/Loading";
+
 
 export default function App() {
   const [load, setLoad] = useState(true);
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoad(false);
-    }, 2500);
+    }, 1500);
     return () => clearTimeout(timer);
   }, []);
   return (
@@ -19,15 +21,15 @@ export default function App() {
           <h1 style={{color:'white'}}>Loading...</h1>
         </div>
       )}
-      <Canvas>
+      <Canvas style={{cursor:'grab' }}>
         <directionalLight intensity={0.5} />
         {load ? (
           <>
-            
             <Loading />
           </>
         ) : (
-          <Suspense fallback={<Loading />}>
+          <Suspense  fallback={<Loading />}>
+            <Background />
             <Model />
           </Suspense>
         )}
